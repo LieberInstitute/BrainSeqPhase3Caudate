d0085d4c65efa69360badc0066418c8182044549
KrotosBenjamin <kj.benjamin90@gmail.com>
2021-08-05 11:37:10 -0400

    adding updated version of heatmap plots for quality control

--- quality_control/heatmap_pcs/_h/heatmap_plots.R
+++ quality_control/heatmap_pcs/_h/heatmap_plots.R
@@ -62,11 +62,29 @@ prep_data <- function(covars, func){
 }
 memEST <- memoise::memoise(prep_data)
 
-fit_model <- function(covars, fnc){
+prep_qsv <- function(fnc){
+    qsv_filename = paste0("/ceph/projects/v4_phase3_paper/inputs/counts/",
+                          "text_files_counts/_m/caudate/qSV_caudate.csv")
+    est_df <- read.csv(qsv_filename, row.names = "X") %>%
+        rownames_to_column("RNum") %>%
+        pivot_longer(!RNum, names_to = "qSV", values_to = "qSV_value") %>%
+        inner_join(fnc(), by="RNum")
+    est_df$PC <- factor(est_df$PC, levels = paste0('PC', 1:15))
+    est_df$qSV <- factor(est_df$qSV, levels = paste0('PC', 1:15))
+    return(est_df)
+}
+memQSV <- memoise::memoise(prep_qsv)
+
+fit_model <- function(covars, fnc, qsv=FALSE){
     ## Calculate p-values
-    est_fit <- memEST(covars, fnc) %>% group_by(Covariate, PC) %>%
-        do(fitEST = broom::tidy(lm(Variable ~ PC_values, data = .))) %>%
-        unnest(fitEST) %>% filter(term == "PC_values") %>%
+    if(qsv){
+        est_fit0 <- memQSV(fnc) %>% group_by(qSV, PC) %>%
+            do(fitEST = broom::tidy(lm(qSV_value ~ PC_values, data = .)))
+    } else {
+        est_fit0 <- memEST(covars, fnc) %>% group_by(Covariate, PC) %>%
+            do(fitEST = broom::tidy(lm(Variable ~ PC_values, data = .)))
+    }
+    est_fit <- est_fit0 %>% unnest(fitEST) %>% filter(term == "PC_values") %>%
         mutate(p.bonf = p.adjust(p.value, "bonf"),
                p.bonf.sig = p.bonf < 0.05,
                p.bonf.cat = cut(p.bonf,
@@ -79,20 +97,29 @@ fit_model <- function(covars, fnc){
     return(est_fit)
 }
 
-tile_plot <- function(covars, fnc, fn, label){
+tile_plot <- function(covars, fnc, fn, label, qsv=FALSE){
     ## Tile plot (heatmap)
     my_breaks <- c(0.05, 0.01, 0.005, 0)
-    tile_plot <- fit_model(covars, fnc) %>%
-        ggplot(aes(x = Covariate, y = PC, fill = log.p.bonf)) +
-        geom_tile(color = "grey") +
-        geom_text(aes(label=ifelse(p.bonf.sig,
-                                   format(round(log.p.bonf,1), nsmall=1), ""),
-                      color=log.p.bonf),
-                  size=3, fontface="bold", show.legend=FALSE) +
+    limits = c(0, 75)
+    if(qsv){
+        xlabel = "qSVs"
+        tile_plot0 <- fit_model(covars, fnc, qsv) %>%
+            ggplot(aes(x = qSV, y = PC, fill = log.p.bonf,
+                       label=ifelse(p.bonf.sig,format(round(log.p.bonf,1),
+                                                      nsmall=1), "")))
+    } else {
+        xlabel = "Covariate"
+        tile_plot0 <- fit_model(covars, fnc, qsv) %>%
+            ggplot(aes(x = Covariate, y = PC, fill = log.p.bonf,
+                       label=ifelse(p.bonf.sig,format(round(log.p.bonf,1),
+                                                      nsmall=1), "")))
+    }
+    tile_plot <- tile_plot0 + geom_tile(color = "grey") +
+        ggfittext::geom_fit_text(contrast = TRUE, aes(fontface="bold")) +
         viridis::scale_color_viridis(option = "magma") +
         viridis::scale_fill_viridis(name="-log10(p-value Bonf)", option="magma",
-                                    direction=-1) +
-        labs(x='Covariate', color="p-value Bonf\nsignificance",
+                                    direction=-1, limits=limits) +
+        labs(x=xlabel, color="p-value Bonf\nsignificance",
              y=paste(label, "Expression (PCs)")) +
         ggpubr::theme_pubr(base_size = 15) +
         theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
@@ -103,11 +130,14 @@ tile_plot <- function(covars, fnc, fn, label){
 covarsCont = memPHENO() %>%
     select(-c("Region", "BrNum", "Protocol", "Sex", "Race", "Dx",
               "antipsychotics", "lifetime_antipsych"))
+
 ## Normalized expression
 tile_plot(covarsCont, memNORM, "continous_norm", "Normalize")
+tile_plot(covarsCont, memNORM, "continous_norm_qSV", "Normalize", TRUE)
 
 ## Residualized expression
 tile_plot(covarsCont, memRES, "continous_res", "Residualized")
+tile_plot(covarsCont, memRES, "continous_res_qSV", "Residualized", TRUE)
 
 #### Reproducibility information ####
 print("Reproducibility information:")
