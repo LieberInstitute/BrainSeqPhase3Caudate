1ba971ade42ecabda299c5f89778cdd19e2ec899
KrotosBenjamin <kj.benjamin90@gmail.com>
2022-03-04 08:49:38 -0500

    adding meta analysis: exons and junctions

--- analysis/eqtl/ancestry_comparison/meta_analysis/_h/step2d.sh
+++ analysis/eqtl/ancestry_comparison/meta_analysis/_h/step2d.sh
@@ -2,5 +2,10 @@
 
 FEATURE="junctions"
 
+# Node 5
 Rscript ../_h/parallel_mashr_junctions.R --feature $FEATURE --run_chunk \
         --chunk_size 1500 --threads 4 1>$FEATURE/output.log 2>$FEATURE/error.log
+
+# # Node 6 or 7
+# Rscript ../_h/parallel_mashr_junctions.R --feature $FEATURE --run_chunk \
+#         --chunk_size 1500 --threads 2 1>$FEATURE/output.log 2>$FEATURE/error.log
